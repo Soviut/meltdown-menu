6abd38e14d3257a715f317c4078df33834c90cfd
Soviut <izamojc@zamtools.com>
2017-08-10 22:38:39 -0400

    Html schedule (#9)
    * started schedule screen
    
    * started schedule screen
    
    * cleaned up redundant source in carousel
    
    * set transition time to 2 minutes
    
    * introduced carousel block
    
    * reintroduced 2 minute delay
    
    * started html schedule
    
    * styled titles
    
    * started schedule icons
    
    * fixed street fighter icon
    
    * implemented divider
    
    * added monday
    
    * added hearthstone and street fighter v to schedule
    
    * added super smash bros melee
    
    * added friday + saturday
    
    * added sunday
    
    * added title
    
    * added padding to schedule screen
    
    * vertically aligned dates

---
+++ static/scripts/carousel.js
@@ -0,0 +1,18 @@
+$(function() {
+  let current = 0
+  let delay = 1000 * 30 // seconds as milliseconds
+
+  let slides = $('.carousel__slide')
+
+  function switchScreen(i) {
+    slides.removeClass('carousel__slide--current')
+    slides.eq(i).addClass('carousel__slide--current')
+  }
+  switchScreen(current)
+
+  setInterval(function() {
+    // wrap index if it exceeds the total number of slides
+    current = current + 1 >= slides.length ? 0 : current + 1
+    switchScreen(current)
+  }, delay)
+})
