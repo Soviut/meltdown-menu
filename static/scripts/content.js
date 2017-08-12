c8cd2990e6b2db5f189aeb2800108c4fd381f8e7
Ian Zamojc <izamojc@pagerduty.com>
2017-08-12 01:13:53 -0400

    started content loading script

---
+++ static/scripts/content.js
@@ -0,0 +1,18 @@
+console.log('ding')
+
+let apiKey = 'AIzaSyCaY54phJEa6e2CtRZtmDt66XsicmmZRas'
+let spreadsheetId = '1SLjvtSUp4aacbUO7_2KymP-_Wtye07JWfEkm-oAdxpA'
+let range = 'menu!A2:C10' // line 1 is column titles, skip
+
+let url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`
+
+$(function() {
+  $.ajax({
+    url: url,
+    method: 'GET',
+    dataType: 'json'
+  })
+  .done(function(res) {
+    console.log(res)
+  })
+})
