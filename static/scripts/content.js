f35927754e73e002ae7b777d4397e33807488dcb
Ian Zamojc <izamojc@pagerduty.com>
2017-08-12 02:31:20 -0400

    added fail state

--- static/scripts/content.js
+++ static/scripts/content.js
@@ -1,5 +1,3 @@
-console.log('ding')
-
 let apiKey = 'AIzaSyCaY54phJEa6e2CtRZtmDt66XsicmmZRas'
 let spreadsheetId = '1SLjvtSUp4aacbUO7_2KymP-_Wtye07JWfEkm-oAdxpA'
 let range = 'menu!A2:C10' // line 1 is column titles, skip
@@ -14,5 +12,8 @@ $(function() {
   })
   .done(function(res) {
     console.log(res)
+  })
+  .fail(function(err) {
+    console.log(err)
   })
 })
