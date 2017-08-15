c5c962af2ac618a47caef7dd960e30a0393a75ee
Soviut <izamojc@zamtools.com>
2017-08-14 22:30:23 -0400

    Google sheets (#12)
    * started content loading script
    
    * added fail state
    
    * refactored data fetching to its own function
    
    * junk
    
    * refactored menu to use vue template
    
    * integrated google sheets data with specials
    
    * integrated google sheets data with schedule
    
    * integrated icons from data

---
+++ static/scripts/content.js
@@ -0,0 +1,55 @@
+let apiKey = 'AIzaSyCaY54phJEa6e2CtRZtmDt66XsicmmZRas'
+let spreadsheetId = '1SLjvtSUp4aacbUO7_2KymP-_Wtye07JWfEkm-oAdxpA'
+
+function fetchData(range) {
+  let url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`
+
+  return $.ajax({
+    url: url,
+    method: 'GET',
+    dataType: 'json'
+  })
+  .done(function(res) {
+    console.log(res)
+  })
+  .fail(function(err) {
+    console.log(err)
+  })
+}
+
+var specials = new Vue({
+  el: '#specials',
+  data: {
+    items: []
+  }
+})
+
+// line 1 of the sheet is column titles, skip by starting at A2
+fetchData('menu!A2:C10').done(function(res) {
+  specials.items = res.values.map(val => {
+    return {
+      title: val[0],
+      price: val[1],
+      description: val[2]
+    }
+  })
+})
+
+var schedule = new Vue({
+  el: '#schedule',
+  data: {
+    items: []
+  }
+})
+
+// line 1 of the sheet is column titles, skip by starting at A2
+fetchData('schedule!A2:F8').done(function(res) {
+  schedule.items = res.values.map(val => {
+    return {
+      day: val[0],
+      title: val[1],
+      subtitle: val[2],
+      icons: val.slice(3)
+    }
+  })
+})
